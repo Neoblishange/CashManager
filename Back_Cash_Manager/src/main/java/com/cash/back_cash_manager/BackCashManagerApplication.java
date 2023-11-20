b617c5649557b1b206f6f3bc9ccf73521cbc0a4b
Flo <florent.sor@epitech.eu>
2023-11-20 17:23:25 +0100

    [FEAT] Setup new backend project in maven java spring boot

---
+++ Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/BackCashManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cash.back_cash_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCashManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackCashManagerApplication.class, args);
+    }
+
+}
---
+++ Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/controllers/ApiController.java
@@ -0,0 +1,14 @@
+package com.cash.back_cash_manager.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
