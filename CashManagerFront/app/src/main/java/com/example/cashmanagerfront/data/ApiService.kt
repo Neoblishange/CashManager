89146ac1d031c6a33c8b6239cdfb76ff5dfc871c
LICARYA\lecom <lecomte.justine@hotmail.com>
2023-12-05 17:29:06 +0100

    WIP api call

---
+++ CashManagerFront/app/src/main/java/com/example/cashmanagerfront/data/ApiService.kt
@@ -0,0 +1,60 @@
+
+package com.example.cashmanagerfront.data
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.accept
+import io.ktor.client.request.request
+import io.ktor.http.HttpMethod
+import io.ktor.client.statement.*
+import io.ktor.http.ContentType
+import io.ktor.util.InternalAPI
+
+
+object ApiService {
+
+    private val client = HttpClient()
+
+    private const val API_BASE_URL = "http://localhost:4000/"
+    private const val AUTH_HEADER = "Authorization"
+
+    @OptIn(InternalAPI::class)
+    suspend fun makeRequest(
+        endpoint: String,
+        method: HttpMethod,
+        token: String? = null,
+        bodyRequest: Any? = null
+    ): HttpResponse {
+        val url = "$API_BASE_URL$endpoint"
+        val headers = mutableMapOf<String, String>()
+
+        token?.let { headers[AUTH_HEADER] = "Bearer $it" }
+
+        return client.request(url) {
+            this.method = method
+            headers.forEach { (key, value) ->
+                this.headers.append(key, value)
+            }
+
+            if (method != HttpMethod.Get && bodyRequest != null) {
+                this.body = bodyRequest
+            }
+        }
+    }
+
+    suspend fun get(endpoint: String, token: String? = null): HttpResponse {
+        return makeRequest(endpoint, HttpMethod.Get, token)
+    }
+
+    suspend fun post(endpoint: String, token: String? = null, body: Any): HttpResponse {
+        return makeRequest(endpoint, HttpMethod.Post, token, body)
+    }
+
+    suspend fun put(endpoint: String, token: String? = null, body: Any): HttpResponse {
+        return makeRequest(endpoint, HttpMethod.Put, token, body)
+    }
+
+    suspend fun delete(endpoint: String, token: String? = null): HttpResponse {
+        return makeRequest(endpoint, HttpMethod.Delete, token)
+    }
+
+}
