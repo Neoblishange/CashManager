ab14f392db20bc05dc1546c864987a7af124c649
Flo <florent.sor@epitech.eu>
2023-12-05 10:44:30 +0100

    debut

---
+++ Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/config/JsonResponse.java
@@ -0,0 +1,4 @@
+package com.cash.back_cash_manager.config;
+
+public record JsonResponse(String message) {
+}
--- Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/controllers/ApiController.java
+++ Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/controllers/ApiController.java
@@ -1,5 +1,7 @@
 package com.cash.back_cash_manager.controllers;
 
+import com.cash.back_cash_manager.config.JsonResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello";
+    @GetMapping("/test")
+    public ResponseEntity<Object> test() {
+        JsonResponse response = new JsonResponse("test");
+        return ResponseEntity.ok().body(response);
     }
 }
+
+//mvn clean install
+//java -jar .\target\Back_Cash_Manager-0.0.1-SNAPSHOT.jar
