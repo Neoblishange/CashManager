1b4855b88c673bf3ec860ca78ef36673c27b9cab
LICARYA\lecom <lecomte.justine@hotmail.com>
2023-12-10 18:27:04 +0100

    WIP read nfc tag

--- CashManagerFront/app/src/main/java/com/example/cashmanagerfront/ui/screens/PayoutNFCScreen.kt
+++ CashManagerFront/app/src/main/java/com/example/cashmanagerfront/ui/screens/PayoutNFCScreen.kt
@@ -1,5 +1,14 @@
 package com.example.cashmanagerfront.ui.screens
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
+import android.nfc.tech.MifareClassic
+import android.nfc.tech.Ndef
+import android.nfc.tech.NdefFormatable
 import androidx.compose.foundation.layout.Column
 
 import androidx.compose.foundation.layout.Spacer
@@ -18,14 +27,24 @@ import com.example.cashmanagerfront.ui.screens.widgets.AppBarWidget
 import com.example.cashmanagerfront.ui.screens.widgets.BackgroundApp
 import com.example.cashmanagerfront.ui.screens.widgets.CustomText
 import com.example.cashmanagerfront.ui.utils.Strings
-
+import android.provider.Settings
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun PayoutNFCScreen(navController: NavHostController, total: String = "90") {
+    val context: Context = LocalContext.current
+    val nfcAdapter = NfcAdapter.getDefaultAdapter(context)
+
     Surface(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
     ) {
+        LaunchedEffect(key1 = true) {
+            isNfcAvailable(context, nfcAdapter)
+        }
         BackgroundApp()
         AppBarWidget(
             navController = navController,
@@ -50,8 +69,68 @@ fun PayoutNFCScreen(navController: NavHostController, total: String = "90") {
                 color = Color.White,
                 size = 40.sp
             )
-
+            TextButton(onClick = {
+                isNfcAvailable(context, nfcAdapter)
+                val yourTagCallback = YourTagCallback()
+                nfcAdapter?.enableReaderMode(context as Activity?, yourTagCallback, NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, null)
+            }) {
+                Text(text = "start nfc scan", color = Color.White)
+            }
+            TextButton(onClick = {
+                nfcAdapter?.disableReaderMode(context as Activity?)
+            }) {
+                Text(text = "stop nfc scan", color = Color.White)
+            }
             Spacer(modifier = Modifier.weight(1f))
+        }
+
+    }
+}
+
+fun isNfcAvailable(context: Context, nfcAdapter: NfcAdapter) {
+    if (nfcAdapter != null && nfcAdapter.isEnabled) {
+        println("if")
+    } else {
+        println("else")
+        val intent = Intent(Settings.ACTION_NFC_SETTINGS)
+        context.startActivity(intent)
+    }
+}
+
+class YourTagCallback : NfcAdapter.ReaderCallback {
+    override fun onTagDiscovered(tag: Tag?) {
+        println("onTagDiscovered")
+        tag?.let {
+            val mifareClassic = MifareClassic.get(it)
+            println("mifare ------> ${mifareClassic }")
+
+            mifareClassic?.let { mifare ->
+                mifare.connect()
+
+                val sectorCount = mifare.sectorCount
+                println("sectorCount ------> $sectorCount")
+
+                println("mifare tag -------> ${mifare.tag} ")
+                println("mifare sectorCount -------> ${mifare.sectorCount} ")
+                println("mifare size -------> ${mifare.size} ")
+                println("mifare isConnected -------> ${mifare.isConnected} ")
+                println("mifare blockCount -------> ${mifare.blockCount} ")
+                for (sectorIndex in 0 until sectorCount) {
+                    if (mifare.authenticateSectorWithKeyA(sectorIndex, MifareClassic.KEY_DEFAULT)) {
+                        val blockCount = mifare.getBlockCountInSector(sectorIndex)
+                        val startBlock = mifare.sectorToBlock(sectorIndex)
+                        val endBlock = startBlock + blockCount
+
+                        for (blockIndex in startBlock until endBlock) {
+                            val data = mifare.readBlock(blockIndex)
+                            val bytesData = String(data)
+                            println("bytesData ------> $bytesData")
+                            println("Données lues depuis le bloc $blockIndex : ${data.contentToString()}")
+                        }
+                    }
+                }
+                mifare.close()
+            }
         }
 
     }
