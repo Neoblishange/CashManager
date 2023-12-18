6d4eeeda31b1ba4783b47f38575f6d8a54cb7c44
Flo <florent.sor@epitech.eu>
2023-12-18 14:17:33 +0100

    [FIT] swagger done + delete useless test controller : "ApiController.java"

--- Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/entities/User.java
+++ Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/entities/User.java
@@ -1,6 +1,8 @@
 package com.cash.back_cash_manager.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,10 +14,14 @@ import lombok.Setter;
 @Entity
 public class User {
     @Id
+    @NotBlank
+    @JsonIgnore
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     @Column(name = "name", unique = true)
     private String name;
+    @NotBlank
     @Column(name = "password")
     private String password;
 }
--- Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/models/Payment.java
+++ Back_Cash_Manager/src/main/java/com/cash/back_cash_manager/models/Payment.java
@@ -1,5 +1,6 @@
 package com.cash.back_cash_manager.models;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Hidden
 public class Payment {
 
     private int accountNumber;
