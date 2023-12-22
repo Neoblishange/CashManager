7366e3fb9ff055507071628a96dc4423f6e654f7
Lecomte Justine <95852275+Licarya@users.noreply.github.com>
2023-12-22 16:37:55 +0100

    Update ApiService.kt

--- CashManagerFront/app/src/main/java/com/example/cashmanagerfront/data/ApiService.kt
+++ CashManagerFront/app/src/main/java/com/example/cashmanagerfront/data/ApiService.kt
@@ -20,48 +20,4 @@ object ApiService {
 
     val transactionRepository: TransactionRepository = retrofit.create(TransactionRepository::class.java)
 
-    /*private val client = HttpClient()
-
-    private const val AUTH_HEADER = "Authorization"
-
-    @OptIn(InternalAPI::class)
-    suspend fun makeRequest(
-        endpoint: String,
-        method: HttpMethod,
-        token: String? = null,
-        bodyRequest: Any? = null
-    ): HttpResponse {
-        val url = "$API_BASE_URL$endpoint"
-        val headers = mutableMapOf<String, String>()
-
-//        token?.let { headers[AUTH_HEADER] = "Bearer $it" }
-
-        return client.request(url) {
-            this.method = method
-            headers.forEach { (key, value) ->
-                this.headers.append(key, value)
-            }
-
-            if (method != HttpMethod.Get && bodyRequest != null) {
-                this.body = bodyRequest
-            }
-        }
-    }
-
-    suspend fun get(endpoint: String, token: String? = null): HttpResponse {
-        return makeRequest(endpoint, HttpMethod.Get, token)
-    }
-
-    suspend fun post(endpoint: String, token: String? = null, body: Any): HttpResponse {
-        return makeRequest(endpoint, HttpMethod.Post, token, body)
-    }
-
-    suspend fun put(endpoint: String, token: String? = null, body: Any): HttpResponse {
-        return makeRequest(endpoint, HttpMethod.Put, token, body)
-    }
-
-    suspend fun delete(endpoint: String, token: String? = null): HttpResponse {
-        return makeRequest(endpoint, HttpMethod.Delete, token)
-    }*/
-
 }
